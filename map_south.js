5480489a3f1dae680b355fe1e210c4a5bfab4ba7
vup0622 <vup0622@gmail.com>
2019-11-29 14:39:30 +0800

    南區地圖 JS 刪icon size

--- map_south.js
+++ map_south.js
@@ -216,8 +216,7 @@ function initMap() {
             latlngset = new google.maps.LatLng(lat, long);
 
             var new_icon = {
-                url: 'https://image.flaticon.com/icons/svg/149/149984.svg',
-                size: new google.maps.Size(80, 80),
+                url: 'https://image.flaticon.com/icons/svg/149/149984.svg', 
                 scaledSize: new google.maps.Size(40, 40),
                 origin: new google.maps.Point(0, 0),
                 anchor: new google.maps.Point(20, 50),
